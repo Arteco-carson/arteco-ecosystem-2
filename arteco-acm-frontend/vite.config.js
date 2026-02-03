f84edac968168c9fe470e43ddaf17c120e5ff24c
Ian - ARTECO <iandgoodall@gmail.com>
2026-02-03 12:19:46 +0000

    chore: update vite config and trigger rebuild for env vars

--- arteco-acm-frontend/vite.config.js
+++ arteco-acm-frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/acm/',
   plugins: [react()],
+  envDir: '..', // <--- This allows ACM to read the single .env file in the root
   server: {
     proxy: {
       '/api': {
